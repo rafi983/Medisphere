f3bafde361d37bc229fbd95602b96f011e09250e
rafi983 <rafiirfan211@gmail.com>
2021-11-26 19:16:00 +0600

    private-route-bug-fixed

--- src/components/ServiceDetail/ServiceDetail.js
+++ src/components/ServiceDetail/ServiceDetail.js
@@ -8,17 +8,17 @@ import NavMenu from "../Navbar/NavMenu";
 const ServiceDetail = () => {
   const { id } = useParams();
 
-  const [servicedetail, setServiceDetail] = useState({});
+  const [serviceDetail, setServiceDetail] = useState({});
   const [services] = useServices();
 
   const history = useHistory();
 
   useEffect(() => {
     const singleService = services?.find(
-      (service) => service.id === Number(id)
+      (service) => service.id === +id
     );
     setServiceDetail(singleService);
-  }, [services, id]);
+  }, [services,id]);
 
   const backToHome = () => {
     history.push("/");
@@ -30,14 +30,14 @@ const ServiceDetail = () => {
       <Container>
         <Row className="d-flex align-items-center mt-5">
           <Col md={6}>
-            <h1>{servicedetail?.title}</h1>
-            <p>{servicedetail?.desc}</p>
+            <h1>{serviceDetail?.title}</h1>
+            <p>{serviceDetail?.desc}</p>
             <Button variant="info" className="px-4" onClick={backToHome}>
               Back
             </Button>
           </Col>
           <Col md={6} className="mt-3">
-            <Image src={servicedetail?.img} className="border border-5" fluid />
+            <Image src={serviceDetail?.img} className="border border-5" fluid />
           </Col>
         </Row>
       </Container>
@@ -47,3 +47,5 @@ const ServiceDetail = () => {
 };
 
 export default ServiceDetail;
+
+
